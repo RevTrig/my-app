ecb5121e482ff268b05887bab9df6b4bca630071
RevTrig <84313651+RevTrig@users.noreply.github.com>
2021-06-01 10:30:10 +0200

    Create logo.js

---
+++ src/components/Logo/logo.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './logo.css';
+
+class Logo extends Component {
+  
+  render() {
+    return (
+      <div className="logo-main">
+          <img src="adidas_logo.png" class ="size"/>
+
+      </div>
+    )
+  }
+}
+
+export default Logo;
