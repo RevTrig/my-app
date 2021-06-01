c96ddffe7b8f8a8bc823137c15e55ad495da1cca
RevTrig <84313651+RevTrig@users.noreply.github.com>
2021-06-01 10:59:25 +0200

    Add files via upload

--- src/components/Logo/logo.test.js
+++ src/components/Logo/logo.test.js
@@ -1,1 +1,12 @@
 //test Logo
+import { render, screen } from '@testing-library/react';
+import Logo from './logo';
+
+describe('Logo', () => {
+  test('Logo must have src = "adidas_logo.png" and class="size"', () => {
+    render(<Logo/>);
+    const logo = screen.getByRole('img');
+    expect(logo).toHaveAttribute('src', 'adidas_logo.png');
+    expect(logo).toHaveAttribute('class', 'size');
+  });
+});
